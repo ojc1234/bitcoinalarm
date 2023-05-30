2a07c214a9c079929ae6ff6424fa7b3bfe3d9edb
노니 <ojc123004@gmail.com>
2023-05-30 16:39:01 +0900

    main

--- src/component/ajax_botton/button.js
+++ src/component/ajax_botton/button.js
@@ -1,22 +1,24 @@
 import React from "react"
 import axios from "axios";
-export default function hello(props)
-{
-    return(
-      
-        <button
-        onClick={() => {setInterval(()=>{ axios
-          .get("https://api.bithumb.com/public/ticker/ALL_KRW")
+export default function hello(props) {
+  return (
+
+    <button
+      onClick={() => {
+        setInterval(() => {
+          axios
+            .get("https://api.bithumb.com/public/ticker/ALL_KRW")
           .then((value) => {
             props.stateSet({ price: value });
           })
           .catch(() => {
             props.stateSet({ price: 0 });
-          });},100)
-         
-        }}
-      >
-        클릭
-      </button>
-    )
+          });
+        }, 100)
+
+      }}
+    >
+      클릭
+    </button>
+  )
 }
--- src/component/bitcoindetail/bitcoindetail.js
+++ src/component/bitcoindetail/bitcoindetail.js
@@ -7,15 +7,15 @@ function bitcoindetail(props) {
       <div>{props.coinname} 현제정보</div>
       <div>
         <ul>
-          <li>최고가:90</li>
-          <li>최저가:80</li>
+          <li>최고가:{props.price ? props.price.data["data"][props.symbol]["max_price"]:0}원</li>
+          <li>최저가:{props.price ? props.price.data["data"][props.symbol]["min_price"]:0}원</li>
         </ul>
       </div>
       <div>
         <a href="https://www.naver.com">자세히 보기</a>
       </div>
       <div>
-        {props.price ? props.price.data["data"][props.symbol]["closing_price"]:0}
+        {props.price ? props.price.data["data"][props.symbol]["closing_price"]:0}원
         {/*props.price[1]["BTC"]["closing_price"]*/}
       </div>
 
